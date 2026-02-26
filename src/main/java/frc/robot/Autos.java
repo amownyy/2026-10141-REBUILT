883c1e059402291c4f7b741e0713b41feb3d2cda
Emre <amowny08@gmail.com>
2026-02-26 20:21:29 +0300

    Update Autos.java

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -13,6 +13,14 @@ import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.commands.EjectTimed;
 import frc.robot.commands.IntakeTimed;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ *
+ * @author Team 6045
+ */
 @SuppressWarnings("unused")
 public class Autos {
     @SuppressWarnings("FieldMayBeFinal")
